3b1a571e3c53c600a7590cea0e5a2864740ce58a
kkemot <tomasz.kondel@gmail.com>
2018-12-11 14:29:49 +0100

    Add site-packages path to plugin

--- plugin.py
+++ plugin.py
@@ -26,10 +26,7 @@
 
 import os
 import sys
-
-module_paths = [x[0] for x in os.walk( os.path.join(os.path.dirname(__file__), '.', '.env/lib/') ) if x[0].endswith('site-packages') ]
-for mp in module_paths:
-    sys.path.append(mp)
+sys.path.append('/usr/lib/python3.6/site-packages')
 
 import Domoticz
 import msgpack
